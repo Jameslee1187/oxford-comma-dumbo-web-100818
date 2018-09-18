242d083d0bbd2ea6cc8a13faa2c45b960ee98aaa
jameslee1187 <jameslee1187@gmail.com>
2018-09-18 14:53:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,4 +5,16 @@ def oxford_comma(array)
   if array.length == 2
     return array.join(" and ")
   end
+  if array.length >= 3
+    array.pop
+    array.push(" and #{array[-1]}")
+    return array.join(" , ")
+
+
+    #     elsif array.length >= 3
+    # new_last_array_item = "and #{array[-1]}"
+    # array.pop
+    # array.push(new_last_array_item)
+    # return array.join(", ")
+  end
 end
