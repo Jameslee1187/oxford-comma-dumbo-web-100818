e1687e2eb465b2d5f4ea0b11d2dc1de4b186d9d4
jameslee1187 <jameslee1187@gmail.com>
2018-09-18 14:54:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,16 +5,13 @@ def oxford_comma(array)
   if array.length == 2
     return array.join(" and ")
   end
-  if array.length >= 3
+
+
+
+        if array.length >= 3
+    new_last_array_item = "and #{array[-1]}"
     array.pop
-    array.push(" and #{array[-1]}")
-    return array.join(" , ")
-
-
-    #     elsif array.length >= 3
-    # new_last_array_item = "and #{array[-1]}"
-    # array.pop
-    # array.push(new_last_array_item)
-    # return array.join(", ")
+    array.push(new_last_array_item)
+    return array.join(", ")
   end
 end
